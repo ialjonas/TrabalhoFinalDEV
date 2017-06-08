4b1a1bc5f44f5e97d9f310c4878874eca66cd400
Lariel <lariel.negreiros@gmail.com>
2017-06-08 20:26:15 -0300

    add telas

--- src/gui/App.java
+++ src/gui/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 		Scene scene = new Scene(root);
 		
 		stage.setScene(scene);
-		stage.setTitle("Leilões");
+		stage.setTitle("Leilão");
 		stage.show();
 	}
 
