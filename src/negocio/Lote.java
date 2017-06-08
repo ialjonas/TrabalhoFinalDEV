ab525cf9333f817748b55d4273f564f86f4cf850
13111586 <ial.silva@acad.pucrs.br>
2017-06-08 20:02:16 -0300

    Criação dos métodos da classe Lote.

--- src/negocio/Lote.java
+++ src/negocio/Lote.java
@@ -1,5 +1,27 @@
 package negocio;
 
+import java.util.ArrayList;
+
 public class Lote {
+
+	private ArrayList<Bem> loteBem;
+	private static Lote instanciaDeLote = null;
+
+	private Lote() {
+		loteBem = new ArrayList<Bem>();
+	}
+
+	public static Lote getInstance() {
+		if (instanciaDeLote == null) {
+			instanciaDeLote = new Lote();
+		}
+		return instanciaDeLote;
+	}
+
+	public boolean cadastrar(Bem b) {
+		return loteBem.add(b);
+	}
+	
+	
 
 }
