093aeb27ecf2826d7fe242579f094a8f3ff2f250
Lariel <lariel.negreiros@gmail.com>
2017-06-08 20:22:16 -0300

    add telas

---
+++ src/gui/tela1Controller.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class tela1Controller  implements Initializable{
+
+	@FXML
+	private Button bt1;
+
+	@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			// TODO Auto-generated method stub
+			
+	}
+	 
+	@FXML
+	void Novo(ActionEvent event) throws IOException {
+		
+	}
+	    
+	
+
+}
