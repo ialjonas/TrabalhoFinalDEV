20dab0ba03b55be40e40577909f3cbb54bc28e0a
13111586 <ial.silva@acad.pucrs.br>
2017-06-08 19:51:00 -0300

    criação da classe Bem.

--- src/negocio/Bem.java
+++ src/negocio/Bem.java
@@ -1,5 +1,44 @@
 package negocio;
 
 public class Bem {
+	private String breveDescricao;
+	private String descricaoCompleta;
+	private int categoria;
+
+	public Bem(String breveDescricao, String descricaoCompleta, int categoria) {
+		this.breveDescricao = breveDescricao;
+		this.descricaoCompleta = descricaoCompleta;
+		this.categoria = categoria;
+	}
+
+	public String getBreveDescricao() {
+		return breveDescricao;
+	}
+
+	public void setBreveDescricao(String breveDescricao) {
+		this.breveDescricao = breveDescricao;
+	}
+
+	public String getDescricaoCompleta() {
+		return descricaoCompleta;
+	}
+
+	public void setDescricaoCompleta(String descricaoCompleta) {
+		this.descricaoCompleta = descricaoCompleta;
+	}
+
+	public int getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(int categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Bem [breveDescricao=" + breveDescricao + ", descricaoCompleta=" + descricaoCompleta + ", categoria="
+				+ categoria + "]";
+	}
 
 }
