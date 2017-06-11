af7d6c874f65fb91661fcbc81b8eba710670a7c3
lariel_Santos <lariel_Santos@W106LNP2J2.poa.amer.dell.com>
2017-06-11 16:08:59 -0300

    +menus +telas

--- src/gui/tela1Controller.java
+++ src/gui/tela1Controller.java
@@ -4,30 +4,77 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.stage.Modality;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-public class tela1Controller  implements Initializable{
+public class tela1Controller implements Initializable{
+	
+	ObservableList<String> itensChoiseLeilao = FXCollections.observableArrayList("Demanda","Oferta");
+	ObservableList<String> itensChoiseLance = FXCollections.observableArrayList("Aberto","Fechado");
+	
+	@FXML
+	private ChoiceBox<String> cbLeilao;
 
 	@FXML
-	private Button btNovo;
+	private ChoiceBox<String> cbLance;
+	
+	@FXML
+	private DatePicker dpInicio;
+
+	@FXML
+	private DatePicker dpFim;
+
+	@FXML
+	private ComboBox<?> cbUser;
+
+	@FXML
+	private TextField tfValor;
+
+	@FXML
+	private MenuItem miUsuario;
+
+	@FXML
+	private MenuItem miLeilao;
+
+	@FXML
+	private MenuItem miBem;
+
+	@FXML
+	private MenuItem miLote;
+
+	@FXML
+	private MenuItem miFechar;
+
+	@FXML
+	private MenuItem miSobre;
+    
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
+		//cbLeilao.setItems(itensChoiseLeilao);
+		//tfValor.setText("teste");
 			
 	}
 	 
 	@FXML
-	void Novo(ActionEvent event) throws IOException {
+	void NovoLeilao(ActionEvent event) throws IOException {
 		try {
 	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("telaNovoLeilao.fxml"));
 	                Parent parentNovoLeilao = (Parent) fxmlLoader.load();
@@ -39,7 +86,64 @@ public class tela1Controller  implements Initializable{
 	           e.printStackTrace();
 	        }
 	}
-	    
 	
+	@FXML
+    void NovoBem(ActionEvent event) {
+		try {
+	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("telaNovoBem.fxml"));
+	                Parent parentNovoBem = (Parent) fxmlLoader.load();
+	                Stage stageNovoBem = new Stage();
+	                stageNovoBem.setScene(new Scene(parentNovoBem));
+	                stageNovoBem.setTitle("Novo Bem");
+	                stageNovoBem.show();
+	        } catch(Exception e) {
+	           e.printStackTrace();
+	        }
+    }
+	
+	@FXML
+    void NovoLote(ActionEvent event) {
+		try {
+	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("telaNovoLote.fxml"));
+	                Parent parentNovoLote = (Parent) fxmlLoader.load();
+	                Stage stageNovoLote = new Stage();
+	                stageNovoLote.setScene(new Scene(parentNovoLote));
+	                stageNovoLote.setTitle("Novo Lote");
+	                stageNovoLote.show();
+	        } catch(Exception e) {
+	           e.printStackTrace();
+	        }
+    }
 
+    @FXML
+    void NovoUsuario(ActionEvent event) {
+    	try {
+	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("telaNovoUsuario.fxml"));
+	                Parent parentNovoUsuario = (Parent) fxmlLoader.load();
+	                Stage stageNovoUsuario = new Stage();
+	                stageNovoUsuario.setScene(new Scene(parentNovoUsuario));
+	                stageNovoUsuario.setTitle("Novo Usuário");
+	                stageNovoUsuario.show();
+	        } catch(Exception e) {
+	           e.printStackTrace();
+	        }
+    }
+	
+    
+    @FXML
+    void Sobre(ActionEvent event) {
+    	Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Sobre");
+		alert.setHeaderText("TF - Dund. Dev. Sw. \nSistema Controle de Leilões");
+		alert.setContentText("Desenvolvido por: \n Guilherme Dohms\n Ial Jonas\n Lariel Negreiros \n \nProfessor: \n Michael Mora");
+		alert.showAndWait();
+    }
+
+	
+	
+	@FXML
+    void Fechar(ActionEvent event) {
+    	Platform.exit();
+    }
+	    
 }
