f85509a07e186358c0198d69eb59e8dd6f14223e
Ial Jonas <Ial Jonas@192.168.1.7>
2017-07-02 12:39:03 -0300

    Identacao da classe.

--- src/negocio/Leilao.java
+++ src/negocio/Leilao.java
@@ -3,12 +3,12 @@ package negocio;
 import java.time.LocalDate;
 
 public class Leilao {
-	private int loteId,leilaoId;
+	private int loteId, leilaoId;
 	private double arremate;
-	private String vencedor,tipo,tipoLance,dataIni, dataFim;
-	
-	public Leilao(int loteId, String dataIni, String dataFim, double arremate,String vencedor,
-			String tipo, String tipoLance) {
+	private String vencedor, tipo, tipoLance, dataIni, dataFim;
+
+	public Leilao(int loteId, String dataIni, String dataFim, double arremate, String vencedor, String tipo,
+			String tipoLance) {
 		super();
 		this.loteId = loteId;
 		this.dataIni = dataIni;
@@ -18,8 +18,8 @@ public class Leilao {
 		this.tipo = tipo;
 		this.tipoLance = tipoLance;
 	}
-	
-	public Leilao(int leilaoId, int loteId, String dataIni, String dataFim, double arremate,String vencedor,
+
+	public Leilao(int leilaoId, int loteId, String dataIni, String dataFim, double arremate, String vencedor,
 			String tipo, String tipoLance) {
 		super();
 		this.leilaoId = leilaoId;
@@ -31,7 +31,7 @@ public class Leilao {
 		this.tipo = tipo;
 		this.tipoLance = tipoLance;
 	}
-	
+
 	public int getLeilaoId() {
 		return leilaoId;
 	}
@@ -39,24 +39,31 @@ public class Leilao {
 	public int getLoteId() {
 		return loteId;
 	}
+
 	public void setLoteId(int loteId) {
 		this.loteId = loteId;
 	}
+
 	public String getDataIni() {
 		return dataIni;
 	}
+
 	public void setDataIni(String dataIni) {
 		this.dataIni = dataIni;
 	}
+
 	public String getDataFim() {
 		return dataFim;
 	}
+
 	public void setDataFim(String dataFim) {
 		this.dataFim = dataFim;
 	}
+
 	public double getArremate() {
 		return arremate;
 	}
+
 	public void setArremate(double arremate) {
 		this.arremate = arremate;
 	}
@@ -64,28 +71,32 @@ public class Leilao {
 	public String getVencedor() {
 		return vencedor;
 	}
+
 	public void setVencedor(String vencedor) {
 		this.vencedor = vencedor;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public String getTipoLance() {
 		return tipoLance;
 	}
+
 	public void setTipoLance(String tipoLance) {
 		this.tipoLance = tipoLance;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Informações do Leilao: Data inicio: " + dataIni + ", data fim: " + dataFim + ", tipo: " + tipo + ", tipoLance: " + tipoLance
-				+ ", Valor arremate: " + arremate + ", Vencedor: " + vencedor +" Id do Lote: " + loteId;
+		return "Informações do Leilao: Data inicio: " + dataIni + ", data fim: " + dataFim + ", tipo: " + tipo
+				+ ", tipoLance: " + tipoLance + ", Valor arremate: " + arremate + ", Vencedor: " + vencedor
+				+ " Id do Lote: " + loteId;
 	}
-	
-	
+
 }
